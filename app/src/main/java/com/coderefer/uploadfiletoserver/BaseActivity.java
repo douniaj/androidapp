e7cc7a43cc3e944f0a0c7eb246bb41cbc3334169
douniaj <douniiajawhar@gmail.com>
2018-05-21 14:37:12 +0000

    adding files

---
+++ app/src/main/java/com/coderefer/uploadfiletoserver/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.coderefer.uploadfiletoserver;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public abstract class BaseActivity extends Activity {
+    protected final static String currentIp = "10.20.3.150";
+
+}
